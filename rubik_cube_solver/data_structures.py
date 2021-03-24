286dffe2df14fafdf661515cbb67dc7c8aa5e5b0
Will Evans <62522222+AuctortiA@users.noreply.github.com>
2021-03-24 18:14:34 +0000

    Update data_structures.py

--- rubik_cube_solver/data_structures.py
+++ rubik_cube_solver/data_structures.py
@@ -113,7 +113,7 @@ class Cube:
         """
         This move method effectively only makes a U move. Other moves can be produced later by performing cube
         rotations that will change which faces this move method will affect.
-        :return: None.
+        :return: None
         """
 
         buffer = self.faces[self.sides[Side.Front]][0].copy()  # Save the front in a buffer as it will be lost otherwise
